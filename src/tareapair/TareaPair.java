057c145d6c1e402effbee500a7a2ddce1c094d9c
juan_ <juan_@FigaroxEver117>
2024-08-29 19:28:24 -0500

    Apartado de la interfaz Gráfica.

--- src/tareapair/TareaPair.java
+++ src/tareapair/TareaPair.java
@@ -11,16 +11,18 @@ public class TareaPair {
 
     public static void main(String[] args) {
 
-        ArrayList<Pair<Persona, Persona>> lista = new ArrayList<>();
+        //ArrayList<Pair<Persona, Persona>> lista = new ArrayList<>();
 
         Persona dato1 = new Persona("Juan", "18");
         Persona dato2 = new Persona("Comprensar", "17/11/2005");
         Pair<Persona, Persona> yo = new Pair<>(dato1, dato2);
-        lista.add(yo);
-
-        for (Pair<Persona, Persona> pair : lista) {
-            System.out.println(pair.toString());
-        }
+        //lista.add(yo);
+        
+        System.out.println(yo.toString());
+        
+        //for (Pair<Persona, Persona> pair : lista) {
+        //    System.out.println(pair.toString());  Esta tambien era una opción
+        //}
         
         
         
